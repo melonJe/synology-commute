998ccf9460868ccb60adde9474a672093a9cd55c
melonJe <jmayermj@gmail.com>
2023-04-23 00:05:23 +0900

    print(message)

--- main.py
+++ main.py
@@ -4,23 +4,9 @@ import uvicorn
 from datetime import datetime
 from dotenv import load_dotenv
 from fastapi import FastAPI, Body
-from pydantic import BaseModel
 from db.db_Helper import Commute
 
 load_dotenv()
-
-
-class Item(BaseModel):
-    token: str = ''
-    channel_id: str = ''
-    channel_name: str = ''
-    user_id: str = ''
-    username: str = ''
-    post_id: str = ''
-    timestamp: int = datetime.timestamp(datetime.now())
-    text: str = ''
-    trigger_word: str = ''
-
 
 app = FastAPI(debug=True)
 
@@ -28,7 +14,6 @@ app = FastAPI(debug=True)
 @app.post("/")
 def read_root(message: str = Body()):
     message = query_string_to_dict(message)
-    print(message)
     if message.get('token') and message.get('token') != os.getenv('SYNOLGY_TOKEN'):
         return
 
@@ -43,12 +28,13 @@ def read_root(message: str = Body()):
         else:
             message[parameter] = urllib.parse.unquote(message[parameter])
 
+    print(message)
     if message['text'] == '출근':
         commute = Commute(username=message['username'], date=message['timestamp'] - message['timestamp'] % 86400,
                           come_at=message['timestamp'] % 86400)
         commute.save()
     elif message['text'] == '퇴근':
-        commute = Commute.update(leave_at=message['timestamp'] % 86400).where(
+        Commute.update(leave_at=message['timestamp'] % 86400).where(
             Commute.username == message['username']
             and Commute.date == message['timestamp'] - message['timestamp'] % 86400).execute()
     else:
