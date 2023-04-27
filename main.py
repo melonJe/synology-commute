68d9ad4655ed6c81e6a14df9b91c000fd6429a55
SangEun Jeong <sejeong@mv-w.com>
2023-04-27 13:42:07 +0900

    message

--- main.py
+++ main.py
@@ -2,56 +2,64 @@ import os
 
 import uvicorn
 import urllib.parse
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Depends
 from datetime import datetime, timedelta
-from app.database.db_Helper import Commute
+from app.database.db_Helper import Commute, BaseModel
 from app.routers import user
+from typing_extensions import Annotated
 
 app = FastAPI(debug=True)
 app.include_router(user.router)
 
 
-@app.post("/")
-def add_commute(message: str = Body()):
-    message = query_string_to_dict(message)
-    print(message)
+class CommuteDto(BaseModel):
+    token: str = ''
+    channel_id: str = ''
+    channel_name: str = ''
+    user_id: str = ''
+    username: str = ''
+    post_id: str = ''
+    time: datetime = datetime.now()
+    text: str = ''
+    trigger_word: str = ''
 
-    if message.get('token') and message.get('token') != os.getenv('SYNOLGY_TOKEN'):
-        return
 
-    if message.get('timestamp'):
-        message['timestamp'] = datetime.fromtimestamp(int(message['timestamp'][:-3])) + timedelta(hours=9)
-    else:
-        message['timestamp'] = datetime.now()
+async def commute_parameters_parser(message: str = Body()):
+    result = dict()
+    for pair in message.split('&'):
+        key, value = pair.split("=")
+        if key == 'timestamp':
+            result['date'] = datetime.fromtimestamp(value[:3])
+            break
+        result[key] = value
 
     for parameter in ('username', 'text', 'trigger_word'):
-        if not message.get(parameter):
-            return
+        if not result.get(parameter):
+            pass
         else:
-            message[parameter] = urllib.parse.unquote(message[parameter])
+            result[parameter] = urllib.parse.unquote(result[parameter])
+    return result
 
+
+@app.post("/api")
+def add_commute(message: Annotated[CommuteDto, Depends(commute_parameters_parser)]):
     print(message)
-    if message['trigger_word'] == '출근':
-        # try 이미 출근 처리 되었을 때
-        commute = Commute(username=message['username'], date=message['timestamp'].date(),
-                          come_at=message['timestamp'].time())
-        commute.save()
-    elif message['trigger_word'] == '퇴근':
-        commute = Commute.update(leave_at=message['timestamp'].time()).where(
-            Commute.username == message['username']
-            and Commute.date == message['timestamp'].date()).execute()
-    else:
+    if message.token != os.getenv('SYNOLGY_TOKEN'):
         return
 
-    print(message['username'], message['timestamp'])
-
-
-def query_string_to_dict(query_string: str):
-    result = dict()
-    for pair in query_string.split('&'):
-        key, value = pair.split("=")
-        result[key] = value
-    return result
+    print(message)
+    if message.trigger_word == '출근':
+        # try 이미 출근 처리 되었을 때
+        commute = Commute(username=message.username, date=message.time.date(), come_at=message.time.time())
+        commute.save()
+    elif message.trigger_word == '퇴근':
+        commute = Commute.update(leave_at=message.time.time()).where(
+            Commute.username == message.username
+            and Commute.date == message.time.date()).execute()
+    else:
+        return
+    print(message.username, message.time)
+    return {message.username, message.time}
 
 
 if __name__ == "__main__":
