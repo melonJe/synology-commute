bfef27bf11af1d61a7cebc89d3488a25a9877499
melonJe <jmayermj@gmail.com>
2023-05-01 14:31:16 +0900

    excel file download

--- cron.py
+++ cron.py
@@ -1,22 +1,56 @@
 import json
-import schedule
-import requests
+import os
 import time
+import requests
+import schedule
 from datetime import datetime
+from dotenv import load_dotenv
+from peewee import JOIN
+
+from app.database.db_Helper import Commute, User
+
+load_dotenv()
 
 
-def commute():
+def alert():
     now = datetime.now()
     if now.weekday() in [5, 6]:
         return
     requests.post(
-        'https://mv-w.com:1112/webapi/entry.cgi?api=SYNO.Chat.External&method=incoming&version=2&token=%22kbBzHLcSleklQKYqWrwsfuDX8KROSFO3ZeTbRuvcexpueVAIgiXwUvKyyW16SBm2%22',
-        'payload=' + json.dumps({'text': f'{now.date()} 출근 업무 보고 부탁드립니다.'}))
+        os.getenv("COMMUTE_CHAT_URL"),
+        "payload=" + json.dumps({"text": f"{now.date()} 출근 보고 부탁드립니다."}),
+    )
 
 
-if __name__ == '__main__':
-    schedule.every().days.at("17:22").do(commute)
-    # schedule.every().days.at("08:40").do(commute)
+def alert_late():
+    now = datetime.now()
+    if now.weekday() in [5, 6]:
+        return
+    commute = (Commute.select(Commute).where(Commute.date == now.date()))
+    predicate = (User.user_id == commute.c.user_id)
+    query = (
+        User.select(User)
+        .join(commute, on=predicate, join_type=JOIN.LEFT_OUTER)
+        .where(commute.c.come_at.is_null())
+    )
+
+    user_id_list = [item.user_id for item in query]
+
+    requests.post(
+        os.getenv("BOT_URL"),
+        "payload="
+        + json.dumps(
+            {
+                "text": f"{now.date()} 출근 보고 부탁드립니다.",
+                "user_ids": user_id_list
+            }
+        ),
+    )
+
+
+if __name__ == "__main__":
+    schedule.every().days.at("08:40").do(alert)
+    schedule.every().days.at("09:25").do(alert_late)
     while True:
         schedule.run_pending()
         time.sleep(1)
