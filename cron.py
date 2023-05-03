d986e3973d57e5cfb60233577d3406f58b8df157
melonJe <jmayermj@gmail.com>
2023-05-03 19:31:55 +0900

    cron modify

--- cron.py
+++ cron.py
@@ -9,11 +9,11 @@ from peewee import JOIN
 from app.database.db_Helper import Commute, User
 
 
-def alert(message: str):
+def alert():
     now = datetime.now()
     if now.weekday() in [5, 6]:
         return
-    requests.post(conf.COMMUTE_CHAT_URL, "payload=" + json.dumps({"text": f"{now.date()} {message} 보고 부탁드립니다."}))
+    requests.post(conf.COMMUTE_CHAT_URL, "payload=" + json.dumps({"text": f"{now.date()} 보고 부탁드립니다."}))
 
 
 def alert_late():
@@ -21,26 +21,17 @@ def alert_late():
     now = datetime.now()
     if now.weekday() in [5, 6]:
         return
-    commute = (Commute.select(Commute).where(Commute.date == now.date()))
+    commute = (Commute.select(Commute.user_id, Commute.come_at, Commute.leave_at, Commute.date)
+               .where(Commute.date == now.date()))
     predicate = (User.user_id == commute.c.user_id)
-    query = (
-        User.select(User)
-        .join(commute, on=predicate, join_type=JOIN.LEFT_OUTER)
-        .where(commute.c.come_at.is_null())
-    )
+    query = (User.select(User.user_id, User.username, User.manager)
+             .join(commute, on=predicate, join_type=JOIN.LEFT_OUTER)
+             .where(commute.c.come_at.is_null())
+             )
 
     user_id_list = [item.user_id for item in query]
-
-    requests.post(
-        conf.BOT_URL,
-        "payload="
-        + json.dumps(
-            {
-                "text": f"출근 보고 부탁드립니다.",
-                "user_ids": user_id_list
-            }
-        ),
-    )
+    print(user_id_list)
+    requests.post(conf.BOT_URL, "payload=" + json.dumps({"text": f"출근 보고 부탁드립니다.", "user_ids": user_id_list}))
 
 
 def excel_file_download():
@@ -48,9 +39,9 @@ def excel_file_download():
 
 
 if __name__ == "__main__":
-    schedule.every().day.at("08:40").do(alert('출근'))
-    schedule.every().day.at("09:25").do(alert_late())
-    schedule.every().day.at("18:00").do(alert('퇴근'))
+    schedule.every().day.at("08:40").do(alert)
+    schedule.every().day.at("09:25").do(alert_late)
+    schedule.every().day.at("18:00").do(alert)
     while True:
         schedule.run_pending()
         time.sleep(1)
