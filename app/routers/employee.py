9584c888a13195e89c2812e56de9ed9339c11e71
SangEun Jeong <sejeong@mv-w.com>
2023-05-06 23:54:10 +0900

    download url

--- app/routers/employee.py
+++ app/routers/employee.py
@@ -41,9 +41,56 @@ def add_commute(token: Annotated[str, Form()], user_id: Annotated[int, Form()], 
             (Commute.update(leave_at=date.time())
              .where(Commute.employee_id == user_id and Commute.date == date.date())
              .execute())
-        send_message(conf.BOT_COMMUTE_URL, [user_id], {"text": f"{date} {trigger_word} 기록 되었습니다."})
+        send_message(conf.BOT_COMMUTE_URL, [user_id], text=f"{date} {trigger_word} 기록 되었습니다.")
     except CustomException as e:
         raise e
     except Exception as e:
         raise CustomException(message=str(e), status_code=500)
     return {username, date}
+
+
+@router.get("/record/{filename}")
+def download_excel_file(filename: str, start_at: Union[str, None] = None, end_at: Union[str, None] = None):
+    # TODO token valid
+
+    employee = (Employee.select(Employee.employee_id, Employee.name, Employee.manager))
+    predicate = (Commute.employee_id == employee.c.employee_id)
+    query = (Commute.select(employee.c.name, Commute.come_at, Commute.leave_at, Commute.date)
+             .join(employee, on=predicate, join_type=JOIN.LEFT_OUTER))
+
+    query = where_time(query, start_at, end_at)
+
+    query_dict = list(query.order_by(Commute.date.asc()).dicts())
+    name_set = set(item['name'] for item in query_dict)
+    df_dict = {name: pd.DataFrame([item for item in query_dict if item['name'] == name]) for name in name_set}
+    return get_excel_file(filename, df_dict)
+
+
+@router.get("/{name}/record/{filename}")
+def download_excel_file(filename: str, name: Union[str, None] = None,
+                        start_at: Union[str, None] = None, end_at: Union[str, None] = None):
+    # TODO token valid
+
+    employee = (Employee.select(Employee.employee_id, Employee.name, Employee.manager))
+    predicate = (Commute.employee_id == employee.c.employee_id)
+    query = (Commute.select(employee.c.name, Commute.come_at, Commute.leave_at, Commute.date)
+             .join(employee, on=predicate, join_type=JOIN.LEFT_OUTER))
+
+    query = query.where(
+        Commute.employee_id == Employee.select(Employee.employee_id).limit(1).where(Employee.name == name).get())
+
+    query = where_time(query, start_at, end_at)
+
+    query_dict = list(query.order_by(Commute.date.asc()).dicts())
+    return get_excel_file(filename, {name: pd.DataFrame(query_dict)})
+
+
+def where_time(query: any, start_at, end_at):
+    if start_at:
+        query = query.where(Commute.date >= datetime.strptime(start_at, '%Y%m%d'))
+    if end_at:
+        query = query.where(Commute.date < datetime.strptime(end_at, '%Y%m%d'))
+    if not start_at and not end_at:
+        start_at = datetime.now().date().replace(day=1) - relativedelta(months=3)
+        query = query.where(Commute.date >= start_at)
+    return query
--- app/routers/file.py
+++ app/routers/file.py
@@ -20,32 +20,6 @@ router = APIRouter(prefix="/files", tags=["files"], responses={404: {"descriptio
 
 
 @router.get("/excel/{filename}")
-def download_excel_file(filename: str, username: Union[str, None] = None,
-                        start_at: Union[str, None] = None, end_at: Union[str, None] = None):
-    # TODO token valid
-
-    employee = (Employee.select(Employee.employee_id, Employee.name, Employee.manager))
-    predicate = (Commute.employee_id == employee.c.employee_id)
-    query = (Commute.select(employee.c.name, Commute.come_at, Commute.leave_at, Commute.date)
-             .join(employee, on=predicate, join_type=JOIN.LEFT_OUTER))
-
-    if username:
-        query = query.where(Commute.employee_id == Employee.select(Employee.employee_id).limit(1).where(
-            Employee.name == username).get())
-    if start_at:
-        query = query.where(Commute.date >= datetime.strptime(start_at, '%Y%m%d'))
-    if end_at:
-        query = query.where(Commute.date < datetime.strptime(end_at, '%Y%m%d'))
-    if not start_at and not end_at:
-        start_at = datetime.now().date().replace(day=1) - relativedelta(months=3)
-        query = query.where(Commute.date >= start_at)
-    query_dict = list(query.order_by(Commute.date.asc()).dicts())
-    name_set = set(item['name'] for item in query_dict)
-    df_dict = {name: pd.DataFrame([item for item in query_dict if item['name'] == name]) for name in name_set}
-    return get_excel_file(filename, df_dict)
-
-
-@router.get("/excel/months/{month}/{filename}")
 def download_excel_file(filename: str, month: Union[int, None] = None):
     employee = (Employee.select(Employee.employee_id, Employee.name, Employee.manager))
     predicate = (Commute.employee_id == employee.c.employee_id)
@@ -73,8 +47,6 @@ def download_excel_for_bot(token: Annotated[str, Form()], text: Annotated[str, F
     # if employee.name != 'mhkim':
     #     raise CustomException(message='have no control over excel file download', status_code=403)
 
-    # TODO USER, MONTH 로 API 나누기
-
     parameter = text.split(' ')
     filename = '_excel.xlsx'
     if len(parameter) >= 4:
@@ -84,24 +56,25 @@ def download_excel_for_bot(token: Annotated[str, Form()], text: Annotated[str, F
     if len(parameter) >= 2:
         filename = parameter[1] + filename
     if len(parameter) == 1:
-        send_message(conf.BOT_COMMUTE_URL, [user_id], file_url=f'{conf.API_URL}/files/excel/excel.xlsx')
+        send_message(conf.BOT_COMMUTE_URL, [user_id], file_url=f'{conf.API_URL}/employees/record/excel.xlsx')
         return True
 
-    file_url = f'{conf.API_URL}/files/excel/{filename}?'
+    file_url = ''
     if len(parameter) >= 2 and parameter[1]:
         if parameter[1].isdecimal():
             send_message(conf.BOT_COMMUTE_URL, [user_id],
-                         file_url=f"{conf.API_URL}/excel/months/{parameter[1]}/{filename}")
+                         file_url=f"{conf.API_URL}/files/excel/{filename}?month={parameter[1]}")
             return True
         else:
-            file_url = file_url + 'username=' + parameter[1]
+            file_url = f'{conf.API_URL}/employees/{parameter[1]}/record/{filename}'
+
     if len(parameter) >= 3 and parameter[2]:
         if len(parameter[2]) != 8:
             raise CustomException(message=f'Invalid formatted `start_at`', status_code=409)
-        file_url = file_url + '&' + 'start_at=' + parameter[2]
+        file_url = file_url + '%26' + 'start_at=' + parameter[2]
     if len(parameter) >= 4 and parameter[3]:
         if len(parameter[3]) != 8:
             raise CustomException(message=f'Invalid formatted `end_at`', status_code=409)
-        file_url = file_url + '&' + 'end_at=' + parameter[3]
+        file_url = file_url + '%26' + 'end_at=' + parameter[3]
     send_message(conf.BOT_COMMUTE_URL, [user_id], file_url=file_url)
     return True
