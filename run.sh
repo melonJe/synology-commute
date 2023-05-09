eb2e7f378a28db73894be22cdc2b119b2ea33812
sejeong <sejeong@mv-w.com>
2023-05-09 22:41:36 +0900

    null=True

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-docker run -d -p 18000:8000 --name mvw-commute --mount type=bind,source="./postgresql",target=/var/lib/postgresql/data mvw-commute:latest
+docker run -d -p 18000:8000 --name commute --mount type=bind,source="./postgresql",target=/var/lib/postgresql/data mvw-commute:latest
 sleep 5s
-docker exec -it mvw-commute "python3 main.py & python3 cron.py"
+docker exec -it commute "python3 main.py & python3 cron.py"
