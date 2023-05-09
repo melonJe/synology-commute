9dd284f231161b15254a046de4093b69b5290d52
sejeong <sejeong@mv-w.com>
2023-05-09 10:10:44 +0900

    commit

--- app/helper/security_helper.py
+++ app/helper/security_helper.py
@@ -1,8 +1,8 @@
 from app.Exceptions.HttpException import CustomException
 
 
-def check_token(token: str, valid_token: str):
+def check_token(token: str, valid_token: list):
     if not token:
         raise CustomException(message='Empty token', status_code=401)
-    if token != valid_token:
+    if token in valid_token:
         raise CustomException(message='Ineligible token', status_code=401)
