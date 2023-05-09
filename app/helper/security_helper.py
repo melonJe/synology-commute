484979fb184b8c6ae996940c587dcdc30185f148
sejeong <sejeong@mv-w.com>
2023-05-10 05:46:27 +0900

    .replace(microsecond=0)

--- app/helper/security_helper.py
+++ app/helper/security_helper.py
@@ -11,12 +11,7 @@ def check_token(token: str, valid_token: list):
         raise CustomException(message='Ineligible token', status_code=401)
 
 
-def api_key_auth(x_api_key: Annotated[str, Header(convert_underscores=True)] = None):
-    if not x_api_key:
-        raise CustomException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            message="Empty x-api-key"
-        )
+def api_key_auth(x_api_key: Annotated[str, Header(convert_underscores=True)]):
     if x_api_key not in config.X_API_KEY:
         raise CustomException(
             status_code=status.HTTP_401_UNAUTHORIZED,
