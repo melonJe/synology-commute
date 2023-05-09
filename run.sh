49fedba1a6d2fc3a1166342545cd7ac3e3cf3fc2
melonJe <jmayermj@gmail.com>
2023-05-09 23:00:04 +0900

    run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 docker run -d -p 18000:8000 --name commute --mount type=bind,source="./postgresql",target=/var/lib/postgresql/data mvw-commute:latest
 sleep 5s
-docker exec -it commute python3 main.py &
-docker exec -it commute python3 alert_schedule.py &
+docker exec commute python3 main.py &
+docker exec commute python3 alert_schedule.py &
