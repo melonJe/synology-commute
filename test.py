eff227ac8b389c5750ebd962353183f7653a8da7
melonJe <jmayermj@gmail.com>
2023-05-09 10:25:29 +0900

    date_time

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
+from datetime import datetime
+
 import config as conf
 
-
-print(conf.OUTGOING_COMMUTE_TOKEN)
-print('G2cKU5840sxHuzlDTsLUWmSMeIrrzz6brvZTuCVkqVI9v6CoWqC4Fzz1qU8s5RMK' in conf.OUTGOING_COMMUTE_TOKEN)
+print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
