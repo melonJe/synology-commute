f240d6a2e04efd5bdfdc9efd5da74c1d08d8e1c0
melonJe <jmayermj@gmail.com>
2023-05-09 22:30:52 +0900

    Dockerfile

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-docker run -d -p 18000:8000 --name mvw-commute --mount type=bind,source="./postgresql",target=/var/lib/postgresql/data mvw-commute:latest "python3 main.py & python3 cron.py"
+docker run -d -p 18000:8000 --name mvw-commute --mount type=bind,source="./postgresql",target=/var/lib/postgresql/data mvw-commute:latest
+sleep 5s
+docker exec -it mvw-commute "python3 main.py & python3 cron.py"
