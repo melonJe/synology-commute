090ae7f12d644ebf8802bce31c8e622964bfd37c
sejeong <sejeong@mv-w.com>
2023-05-10 04:16:00 +0900

    x-api-key

--- app/helper/security_helper.py
+++ app/helper/security_helper.py
@@ -1,4 +1,8 @@
+import config
 from app.Exceptions.HttpException import CustomException
+from fastapi import Depends, HTTPException, status, Header
+from fastapi.security import OAuth2PasswordBearer
+from typing_extensions import Annotated
 
 
 def check_token(token: str, valid_token: list):
@@ -6,3 +10,16 @@ def check_token(token: str, valid_token: list):
         raise CustomException(message='Empty token', status_code=401)
     if token not in valid_token:
         raise CustomException(message='Ineligible token', status_code=401)
+
+
+def api_key_auth(x_api_key: Annotated[str | None, Header(convert_underscores=True)] = None):
+    if not x_api_key:
+        raise CustomException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            message="Empty x-api-key"
+        )
+    if x_api_key not in config.X_API_KEY:
+        raise CustomException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            message="Ineligible Api Key"
+        )
