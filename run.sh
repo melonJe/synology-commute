45d593782326ee50d750e35cbc2040a6ae3c6c7f
melonJe <jmayermj@gmail.com>
2023-05-09 23:03:12 +0900

    run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-docker run -d -p 18000:8000 --name commute --mount type=bind,source="./postgresql",target=/var/lib/postgresql/data mvw-commute:latest
+docker run -d -p 18000:8000 --name commute --mount type=bind,source="./postgre-data",target=/var/lib/postgresql/data mvw-commute:latest
 sleep 5s
 docker exec commute python3 main.py &
 sleep 5s
