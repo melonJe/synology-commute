9f4871e0f0d2bd133e33af169d743e7e11358bca
melonJe <jmayermj@gmail.com>
2023-05-10 04:29:30 +0900

    rm cache

--- app/dto/employee_dto.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class EmployeeDto(BaseModel):
-    name: str
-    manager: bool
--- run.sh
+++
@@ -1,5 +0,0 @@
-docker run -d -p 18000:8000 --name commute --mount type=bind,source="./postgre-data",target=/var/lib/postgresql/data mvw-commute:latest
-sleep 5s
-docker exec commute python3 main.py &
-sleep 5s
-docker exec commute python3 alert_schedule.py &
