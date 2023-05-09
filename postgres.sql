d640bd979b49eda942c675a4a20fdd7a4d441b0a
sejeong <sejeong@mv-w.com>
2023-05-09 18:34:50 +0900

    Dockerfile

--- postgres.sql
+++ postgres.sql
@@ -1,12 +1,12 @@
 CREATE TABLE "commute" (
-  "employee_id" int(11) DEFAULT NULL,
+  "employee_id" serial,
   "date" date DEFAULT NULL,
   "come_at" time DEFAULT NULL,
   "leave_at" time DEFAULT NULL
 );
 
 CREATE TABLE "employee" (
-  "employee_id" int(11) DEFAULT NULL,
+  "employee_id" int8 DEFAULT NULL,
   "name" varchar(100) DEFAULT NULL,
-  "manager" tinyint(1) DEFAULT '0'
+  "manager" boolean DEFAULT '0'
 );
