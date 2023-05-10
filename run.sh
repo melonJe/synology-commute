06ccd220c1e72309a96aacc920903eae488f33a5
melonJe <jmayermj@gmail.com>
2023-05-10 15:51:50 +0900

    docker

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -d -p 18000:8000 --name commute --link mk-mysql:db mvw-commute:latest
+docker run -entrypoint="/bin/bash" -d -p 18000:8000 --name commute --link postgresql:db mvw-commute:latest
