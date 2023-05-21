0b38f1fa66f2d9a35841f29b69314705317f13cd
majaro-asus <jmayermj@gmail.com>
2023-05-21 12:34:32 +0900

    추가 필요 : 마지막주 월요일 매달 1일 기능

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
-import re
+from datetime import datetime, timedelta
 
-print(re.fullmatch(r"\d{2}:\d{2}", '42:12'))
+from dateutil.relativedelta import relativedelta
+
+import config as conf
+from app.helper.synology_chat_helper import send_message
+
+now = datetime.utcnow() + timedelta(hours=9)
+end_at = now.date().replace(day=1)
+start_at = end_at - relativedelta(months=1)
+
+file_url = f'https://webhook.site/d44cb7bb-c61e-42f8-8f76-60437447323f'
+send_message(conf.BOT_COMMUTE_URL, [29], file_url=file_url)
