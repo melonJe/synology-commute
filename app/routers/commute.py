39e7ebefa2c0dce1bcde2d20dea97215b69f17b1
melon <jmayermj@gmail.com>
2023-05-28 19:20:27 +0900

    excel file download

--- app/routers/commute.py
+++ app/routers/commute.py
@@ -31,26 +31,23 @@ def get_commute(employee_id: int, start_at: str, end_at: str):
         raise CustomException(message=f'Invalid formatted `end_at`', status_code=409)
     end_at = datetime.strptime(end_at, '%Y%m%d')
 
-    commute = Commute.select().where(
-        (Commute.employee_id == employee_id) & (Commute.date >= start_at) & (Commute.date < end_at))
+    commute = Commute.select().where((Commute.employee_id == employee_id) & (Commute.date >= start_at) & (Commute.date < end_at))
     return [item.__data__ for item in commute] if commute else None
 
 
 @router.post("/work")
-def add_commute(token: Annotated[str, Form()], user_id: Annotated[int, Form()], username: Annotated[str, Form()],
-                text: Annotated[str, Form()]):
+def add_commute(token: Annotated[str, Form()], user_id: Annotated[int, Form()], username: Annotated[str, Form()], text: Annotated[str, Form()]):
     check_token(token, conf.WORK_TOKEN)
 
     location = time = ''
-    for item in text.strip().split(' '):
-        if item[0] == '@':
-            location = item[1:]
-        if item[0] == '*':
-            if re.fullmatch(r"\d{2}:\d{2}", item[1:]) and int(item[1:3]) < 24 and int(item[4:]) < 60:
-                time = item[1:]
-            else:
-                send_message(conf.BOT_COMMUTE_URL, [user_id], text=f"시간 포멧이 잘못되었습니다. hh:mm")
-                raise CustomException(message=f"시간 포멧이 잘못되었습니다. hh:mm", status_code=400)
+    for item in text.strip().split(' ')[1:]:
+        if re.fullmatch(r"@?[a-zA-Zㄱ-힣]*", item):
+            location = item[1:].replace('@', '')
+        if re.fullmatch(r"\*?\d{2}:\d{2}", item):
+            time = item.replace('*', '')
+
+    if int(time[:2]) >= 24 or int(time[3:]) >= 60:
+        raise CustomException(message=f"시간 포멧이 잘못되었습니다. hh:mm", status_code=400)
 
     date_time = datetime.utcnow() + timedelta(hours=9)
 
@@ -62,29 +59,24 @@ def add_commute(token: Annotated[str, Form()], user_id: Annotated[int, Form()], 
     try:
         if commute:
             if not (location or time):
-                send_message(conf.BOT_COMMUTE_URL, [user_id],
-                             text=f"장소 : {commute.location}\n시간 : {commute.come_at}\n이미 기록되었습니다.")
+                send_message(conf.BOT_COMMUTE_URL, [user_id], text=f"장소 : {commute.location}\n시간 : {commute.come_at}\n이미 기록되었습니다.")
                 raise CustomException(message=f'already record {commute.come_at}', status_code=409)
             if location:
                 commute.location = location
             if time:
                 commute.come_at = time
             commute.save()
-            send_message(conf.BOT_COMMUTE_URL, [user_id],
-                         text=f"장소 : {commute.location}\n시간 : {commute.come_at}\n출근 기록이 수정되었습니다.")
+            send_message(conf.BOT_COMMUTE_URL, [user_id], text=f"장소 : {commute.location}\n시간 : {commute.come_at}\n출근 기록이 수정되었습니다.")
         else:
             if not location:
                 location = '궁동'
 
             if time:
-                Commute.create(employee_id=user_id, date=date_time.date(), location=location,
-                               come_at=time)
+                Commute.create(employee_id=user_id, date=date_time.date(), location=location, come_at=time)
                 send_message(conf.BOT_COMMUTE_URL, [user_id], text=f"장소 : {location}\n시간 : {time}\n출근 시간이 기록되었습니다.")
             else:
-                Commute.create(employee_id=user_id, date=date_time.date(), location=location,
-                               come_at=date_time.time().replace(microsecond=0))
-                send_message(conf.BOT_COMMUTE_URL, [user_id],
-                             text=f"장소 : {location}\n시간 : {date_time.time().replace(microsecond=0)}\n출근 시간이 기록되었습니다.")
+                Commute.create(employee_id=user_id, date=date_time.date(), location=location, come_at=date_time.time().replace(microsecond=0))
+                send_message(conf.BOT_COMMUTE_URL, [user_id], text=f"장소 : {location}\n시간 : {date_time.time().replace(microsecond=0)}\n출근 시간이 기록되었습니다.")
 
     except CustomException as e:
         raise e
@@ -94,8 +86,7 @@ def add_commute(token: Annotated[str, Form()], user_id: Annotated[int, Form()], 
 
 
 @router.post("/leave")
-def add_commute(token: Annotated[str, Form()], user_id: Annotated[int, Form()], username: Annotated[str, Form()],
-                text: Annotated[str, Form()]):
+def add_commute(token: Annotated[str, Form()], user_id: Annotated[int, Form()], username: Annotated[str, Form()], text: Annotated[str, Form()]):
     check_token(token, conf.LEAVE_TOKEN)
 
     time = ''
--- app/routers/employee.py
+++ app/routers/employee.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter, Depends
 from datetime import datetime
 from app.Exceptions.HttpException import CustomException
 from app.dto.employee import CreateEmployeeDto, UpdateEmployeeDto
-from app.helper import employee_helper
+from app.helper import employee_helper, sql_helper
 from app.helper.db_helper import Commute, Employee
 from typing import Union
 from app.helper.file_helper import get_excel_file
@@ -40,16 +40,15 @@ def delete_employee(employee_id: int):
 
 
 @router.get("/record/{filename}")
-def download_excel_file(filename: str, start_at: Union[str, None] = None, end_at: Union[str, None] = None):
+def download_record_file(filename: str, start_at: Union[str, None] = None, end_at: Union[str, None] = None):
     # TODO token valid
 
     employee = (Employee.select(Employee.employee_id, Employee.name, Employee.manager))
     predicate = (Commute.employee_id == employee.c.employee_id)
-    query = (Commute.select(employee.c.name.alias('이름'), Commute.come_at.alias('출근'), Commute.leave_at.alias('퇴근'),
-                            Commute.date.alias('날짜'))
+    query = (Commute.select(employee.c.name.alias('이름'), Commute.come_at.alias('출근'), Commute.leave_at.alias('퇴근'), Commute.date.alias('날짜'))
              .join(employee, on=predicate, join_type=JOIN.LEFT_OUTER))
 
-    query = where_time(query, start_at, end_at)
+    query = sql_helper.where_time(query, start_at, end_at)
 
     query_dict = list(query.order_by(Commute.date.asc()).dicts())
     name_set = set(item['이름'] for item in query_dict)
@@ -58,31 +57,18 @@ def download_excel_file(filename: str, start_at: Union[str, None] = None, end_at
 
 
 @router.get("/{name}/record/{filename}")
-def download_excel_file(filename: str, name: Union[str, None] = None,
-                        start_at: Union[str, None] = None, end_at: Union[str, None] = None):
+def download_record_file_one_employees(filename: str, name: Union[str, None] = None, start_at: Union[str, None] = None, end_at: Union[str, None] = None):
     # TODO token valid
 
     employee = (Employee.select(Employee.employee_id, Employee.name, Employee.manager))
     predicate = (Commute.employee_id == employee.c.employee_id)
-    query = (Commute.select(employee.c.name.alias('이름'), Commute.come_at.alias('출근'), Commute.leave_at.alias('퇴근'),
-                            Commute.date.alias('날짜'))
+    query = (Commute.select(employee.c.name.alias('이름'), Commute.come_at.alias('출근'), Commute.leave_at.alias('퇴근'), Commute.date.alias('날짜'))
              .join(employee, on=predicate, join_type=JOIN.LEFT_OUTER))
 
     query = query.where(
         Commute.employee_id == Employee.select(Employee.employee_id).limit(1).where(Employee.name == name).get())
 
-    query = where_time(query, start_at, end_at)
+    query = sql_helper.where_time(query, start_at, end_at)
 
     query_dict = list(query.order_by(Commute.date.asc()).dicts())
     return get_excel_file(filename, {name: pd.DataFrame(query_dict)})
-
-
-def where_time(query: any, start_at, end_at):
-    if start_at:
-        query = query.where(Commute.date >= datetime.strptime(start_at, '%Y%m%d'))
-    if end_at:
-        query = query.where(Commute.date < datetime.strptime(end_at, '%Y%m%d'))
-    if not start_at and not end_at:
-        start_at = datetime.now().date().replace(day=1) - relativedelta(months=3)
-        query = query.where(Commute.date >= start_at)
-    return query
